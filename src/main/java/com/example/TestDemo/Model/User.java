5262790c7242965c2a89bf0ac75d82fd021d76f6
JoseLeviRivera <joselevirivera@gmail.com>
2023-07-13 15:27:54 -0600

    Se corrigui persistencia a la base de datos y errores de conexion

--- src/main/java/com/example/TestDemo/Model/User.java
+++ src/main/java/com/example/TestDemo/Model/User.java
@@ -1,15 +1,20 @@
 package com.example.TestDemo.Model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
 
 @Data
 @Entity
-public class User {
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class User implements Serializable {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
